f56e8053a1868f5f0a12300d51fefb2b2b610402
Umberto Peserico <umberto.peserico@gmail.com>
2016-11-23 23:59:18 +0100

    Add comments

--- public/javascripts/helpers/homePage.js
+++ public/javascripts/helpers/homePage.js
@@ -1,8 +1,14 @@
+/*
+ * This function implements how to toggle the form
+ */
 function toggleEmployeeForm() {
     var fieldset = document.getElementById('employeeFieldset');
     fieldset.style.display = (fieldset.style.display == 'block' ? "none" : "block");
 }
 
+/*
+ * This function implements how to clean the form to insert data
+ */
 function cleanEmployeeForm() {
     var form = document.getElementById('employeeForm');
     var formInputs = form.querySelectorAll('input');
--- routes/employees.js
+++ routes/employees.js
@@ -7,22 +7,27 @@ router.get('/', function (req, res, next) {
     employeesCtrl.index(req, res);
 });
 
+/* GET new employee form. */
 router.get('/new', function (req, res, next) {
     employeesCtrl.new(req, res);
 });
 
+/* POST create employee form. */
 router.post('/create', function (req, res, next) {
     employeesCtrl.create(req, res);
 });
 
+/* GET edit employee form. */
 router.get('/edit/:id', function (req, res, next) {
     employeesCtrl.edit(req, res);
 });
 
+/* POST update employee. */
 router.post('/update/:id', function (req, res, next) {
     employeesCtrl.update(req, res);
 });
 
+/* GET delete employee. */
 router.get('/destroy/:id', function (req, res, next) {
     employeesCtrl.destroy(req, res);
 });
