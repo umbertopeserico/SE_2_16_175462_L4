a8475443a6b277751022f3ecdda9e6936f00e340
Umberto Peserico <umberto.peserico@gmail.com>
2016-11-23 21:00:13 +0100

    Add client side application structure

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,5 +1,9 @@
+var Employee = require('../models/employee.js');
+
 function home(request, response) {
-    response.render('homePage');
+    response.render('homePage', {
+        employee: new Employee
+    });
 }
 
 module.exports.home = home;
---
+++ public/javascripts/controllers/homePage.js
@@ -0,0 +1,4 @@
+function toggleEmployeeFormAction() {
+    toggleEmployeeForm();
+    cleanEmployeeForm();
+}
