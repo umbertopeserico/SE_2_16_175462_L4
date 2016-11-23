2f50ba0bdd5f78b5b0ec6a15da596281f43cc5b1
Umberto Peserico <umberto.peserico@gmail.com>
2016-11-23 20:16:48 +0100

    Remove delete and put method from employee destroy and update

--- app.js
+++ app.js
@@ -22,16 +22,6 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var methodOverride = require('method-override')
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        // look in urlencoded POST bodies and delete it
-        var method = req.body._method
-        delete req.body._method
-        return method
-    }
-}));
-
 var indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
 
